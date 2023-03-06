f60cb49641ce8b68926cd9403d37d28181423f69
hugocasildo <hugo598@gmail.com>
2023-03-06 15:34:40 -0600

    Inicial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+import { GlueClient, GetSchemaCommand } from "@aws-sdk/client-glue";
+
+const client = new GlueClient({ region: "us-east-2" });
+const params = {
+  SchemaId: {
+    RegistryName: "clickstreams",
+    SchemaName: "checkoutStarted",
+  },
+};
+
+const command = new GetSchemaCommand(params);
+
+try {
+  const data = await client.send(command)
+  console.log('DATA', data)
+  
+} catch(e) {
+  console.log('Error', e)
+}
+
